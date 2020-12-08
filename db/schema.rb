639afe20933fe63decfa6076cecd2e3375dd4c0f
rocionadal <rocioonadal@gmail.com>
2020-12-08 14:57:40 +0100

    add participants to table bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_07_161411) do
+ActiveRecord::Schema.define(version: 2020_12_08_135532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_12_07_161411) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "participants"
     t.index ["iteration_id"], name: "index_bookings_on_iteration_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
