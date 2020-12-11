e725d1ee4be8ebd5a250a0046cfaf12e1e8eca8a
Dani Diestre Tomas <dani.diestre@gmail.com>
2020-12-11 16:40:02 +0100

    fix card duplication for recetn eent

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -17,6 +17,8 @@ class Booking < ApplicationRecord
 
 
   def assign_iteration
+    return if iteration
+    
     iterations = Iteration.all
     iterations = iterations.where(start_date: start_date..end_date)
     iterations = iterations.joins(:event).where.not(events: { category_id: exclude_category_ids  })
