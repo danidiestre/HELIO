58542d71ceb5decc1a010473748e84a0329415e5
rocionadal <rocioonadal@gmail.com>
2020-12-11 13:13:37 +0100

    add javascript countdown in dasboard cards

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,10 +29,12 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { initFlatpickr } from '../plugins/init_flatpickr';
 import { initSlider } from '../plugins/init_rangeslider';
+import { countDown } from '../functions/countdown';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initFlatpickr();
   initSlider();
+  countDown();
 });
