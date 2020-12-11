be0d292b1ad6b7d39bda8d83d6d028e5d967b9f8
Bruna Scopel <brunascomor@gmail.com>
2020-12-11 11:44:36 +0100

    fixes slider and budget page alignment

--- app/javascript/plugins/init_rangeslider.js
+++ app/javascript/plugins/init_rangeslider.js
@@ -1,14 +1,18 @@
-import "rangeslider.js"
+import rangesliderJs from 'rangeslider-js'
 
 const initSlider = () => {
-    $('input[type="range"]').rangeslider({ 
-      polyfill: false,
-      onSlide: (position, value) => {
+  rangesliderJs.create(
+    document.querySelectorAll('input[type="range"]'),
+    { 
+      min: 40,
+      max: 120, 
+      value: 60, 
+      step: 5,
+      onSlide: (value, position) => {
         document.getElementById("budgetJs").innerText = value
       }
-    });
+    }
+  )
+};
 
-    
-  };
-  
-  export { initSlider };
+export { initSlider };
