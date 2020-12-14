d6242af802d5a3423e8c825874f0c66a2c99cdc3
Dani Diestre Tomas <dani.diestre@gmail.com>
2020-12-14 16:26:47 +0100

    changes in the filter in index page of events

--- app/javascript/controllers/events_controller.js
+++ app/javascript/controllers/events_controller.js
@@ -4,22 +4,52 @@ export default class extends Controller {
     static targets = [ "listItems" ]
 
     filter(e) {
-        this.hideItems(e.target.dataset.category)
-        console.log(e)
+        if (this.categories.includes(e.target.dataset.category)){
+            this.categories = this.categories.filter((value) => {
+               return value != e.target.dataset.category
+            })
+            e.target.style.backgroundColor = '#F4F4F4'
+            e.target.style.color = '#01a0ba'
+
+            
+        }else{
+            this.categories.push(e.target.dataset.category)
+            e.target.style.backgroundColor = '#01a0ba'
+            e.target.style.color = 'white'
+            
+
+        }
+
+        if (this.categories.length === 0) {
+            this.reset()
+        } else {
+            this.hideItems()
+
+        }
         
-        e.target.classList ="button-clicked"
+        console.log(this.categories)
+
     }
 
     hideItems() {
         this.listItemsTargets.forEach(item => {
-            if (item.dataset.category === category_id) {
+            if (this.categories.includes(item.dataset.category)) {
                 item.classList.remove("hide-item")
             } else {
                 item.classList.add("hide-item")
+
             }
         })
     }
 
+    reset(){
+        this.listItemsTargets.forEach(item => {
+            item.classList.remove("hide-item")
+        })
+    }
+    
 
-
+    initialize() {
+        this.categories = []
+    }
 }
