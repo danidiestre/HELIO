5bebc037b14c1d120dd065aa816b7885c1a9f002
rocionadal <rocioonadal@gmail.com>
2020-12-15 11:53:35 +0100

    link progressbar with corresponding form

--- app/javascript/plugins/init_rangeslider.js
+++ app/javascript/plugins/init_rangeslider.js
@@ -3,10 +3,10 @@ import rangesliderJs from 'rangeslider-js'
 const initSlider = () => {
   rangesliderJs.create(
     document.querySelectorAll('input[type="range"]'),
-    { 
+    {
       min: 40,
-      max: 120, 
-      value: 60, 
+      max: 120,
+      value: 60,
       step: 5,
       onSlide: (value, position) => {
         document.getElementById("budgetJs").innerText = value
