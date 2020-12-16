796c144d6f830e53f334a254ae8f0f14d9e92625
Bruna Scopel <brunascomor@gmail.com>
2020-12-16 12:54:15 +0100

    fixes encoding on PDFs

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -9,6 +9,7 @@
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
 WickedPdf.config = {
+  encoding: 'utf8'
   # exe_path: '/usr/local/bin/wkhtmltopdf',
   # enable_local_file_access: true
   # Path to the wkhtmltopdf executable: This usually isn't needed if using
