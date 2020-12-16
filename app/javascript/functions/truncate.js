fc7f0a833012a3c019ee04607b641f00dfbcf777
rocionadal <rocioonadal@gmail.com>
2020-12-16 17:13:05 +0100

    javascript for read more

--- app/javascript/functions/truncate.js
+++ app/javascript/functions/truncate.js
@@ -1,10 +1,17 @@
 const readMore = () => {
   const button = document.querySelector(".read-more");
   const element = document.getElementById("event-description");
-  const description = element.dataset.description;
 
-  button.addEeventListener("click", (element1) => {
-    element1.innerHTML = description
+
+  button.addEventListener("click", (event) => {
+    const description = element.dataset.description;
+    element.dataset.description = element.innerText
+    element.innerHTML = description;
+    if ( button.innerHTML === "Read more" ) {
+      button.innerHTML = "Read less"
+    } else {
+      button.innerHTML = "Read more";
+    }
   })
 }
 
