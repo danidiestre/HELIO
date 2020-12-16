ba734ac5134596a9a2a6b06d48a1b6f84dba5ed9
Jaume Ros <jaumeros77@gmail.com>
2020-12-16 12:30:22 +0100

    now you dont need to login to access past event index and show pages

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
     def show
         @event = Event.find(params[:id])
         @reviews = @event.reviews
